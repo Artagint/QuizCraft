3173aadf859772559a527fb5910b6078b1455373
Artem Tagintsev <artemtagintsev@gmail.com>
2025-03-30 22:06:13 -0700

    linked to firebase, made login UI

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+// FIREBASE PLUGIN
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
